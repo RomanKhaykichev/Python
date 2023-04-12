6349e72241de16bdd125e5667b8d04260eea74d4
Roman_Khaykichev <mr.romanx@mail.ru>
2023-04-12 20:41:28 +0300

    Добавлено альтернативное решение

--- Homework/Seminar_1/Task_4.py
+++ Homework/Seminar_1/Task_4.py
@@ -11,13 +11,23 @@
 
 print("---Задача 4: Найти сколько журавликов сделал каждый ребенок.---")
 
+# Version_1
 total_toys = int(input("Введите колличество журавликов: "))
 katja_toys = total_toys*2/3
 petja_toys = (total_toys - katja_toys)/2
 sereja_toys = petja_toys
 
 if katja_toys == int(katja_toys) and petja_toys == int(petja_toys):
-    print(f"Общее {(total_toys)} -> Петя = {petja_toys} Катя = {katja_toys} Сергей = {sereja_toys}")
+    print(
+        f"Общее {(total_toys)} -> Петя = {petja_toys} Катя = {katja_toys} Сергей = {sereja_toys}")
 else:
     print("нельзя определить")
 print("---")
+
+# Version_2
+input("Version_2: press Enter...")
+total_toys_2 = int(input("Введите колличество журавликов: "))
+if total_toys_2 % 6 == 0:
+    print(f"Общее {(total_toys_2)} -> Петя = {total_toys_2/6} Катя = {total_toys_2*2/3} Сергей = {total_toys_2/6}")
+else:
+    print("нельзя определить")
