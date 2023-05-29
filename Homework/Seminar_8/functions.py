4738d243eb574dad99f2e9bde473aa5b780906b3
Roman_Khaykichev <mr.romanx@mail.ru>
2023-05-29 21:39:47 +0300

    add homework Seminar_8

--- Homework/Seminar_8/functions.py
+++ Homework/Seminar_8/functions.py
@@ -26,3 +26,27 @@ def search(book: str, info: str) -> list[str]:
     book = book.split('\n')
     return list(filter(lambda contact: info.lower() in contact.lower(), book))
     # return [contact for contact in book if info.lower() in contact.lower(),book]
+
+def change_data() -> None:
+    """Изменяет результат поиска по справочнику."""
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+    contact_to_change = input('Введите, что хотите изменить:  ')
+    contact_to_change = search(data, contact_to_change)
+    print(contact_to_change)
+    fio = input('Введите ФИО: ')
+    phone_num = input('Введите номер телефона: ')
+    data[data.index(contact_to_change)] = f'{fio} | {phone_num}'
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
+
+def delete_data() -> None:
+    """Удаляет результат поиска по справочнику."""
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+    contact_to_delete = input('Введите, что хотите удалить:  ')
+    print(contact_to_delete)
+    contact_to_delete = search(data, contact_to_delet)
+    data.remove(contact_to_delete)
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
--- Homework/Seminar_8/main.py
+++ Homework/Seminar_8/main.py
@@ -12,7 +12,7 @@
 import functions
 
 while True:
-    print('1. вывод, 2. добавление, 3. поиск')
+    print('Help => 1. вывод 2. добавление 3. поиск 4. изменение 5. удаление')
     mode = int(input())
     if mode == 1:
         functions.show_data()
@@ -20,5 +20,9 @@ while True:
         functions.add_data()
     elif mode == 3:
         functions.find_data()
+    elif mode == 4:
+        functions.change_data()
+    elif mode == 5:
+        functions.delete_data()    
     else:
         break
